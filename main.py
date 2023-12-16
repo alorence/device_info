19f13aef8ac529ab047a55b0a65406d7053ee52f
Antoine Lorence <alorence@flokod.com>
2023-12-16 22:08:46 +0100

    Allow for raw result from an API

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import logging
 import asyncio
-from typing import Callable, Iterable
-from dataclasses import dataclass
+import logging
 import urllib.parse
+from dataclasses import dataclass
 from json import JSONDecodeError
+from typing import Callable, Iterable
 
-from aiohttp import ClientSession, ClientConnectionError
-
+from aiohttp import ClientSession, ClientConnectionError, ClientResponse
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -18,7 +17,12 @@ log = logging.getLogger()
 @dataclass
 class RemoteApiConfig:
     url: str
-    extractor: Callable[[dict], str]
+    extractor: Callable[[dict], str] = None
+
+    async def extract_result(self, result: ClientResponse) -> str:
+        if self.extractor:
+            return self.extractor(await result.json())
+        return await result.text()
 
 
 ipv4_configs = (
@@ -30,21 +34,20 @@ ipv4_configs = (
 ipv6_configs = (
     RemoteApiConfig("https://ipv6.monipv6.org/json", lambda json_res: json_res["ipaddress"]),
     RemoteApiConfig("https://api-bdc.net/data/client-ip", lambda json_res: json_res["ipString"]),
-    # This one returns a raw value
-    # RemoteApiConfig("https://ifconfig.me", lambda r: r),
+    RemoteApiConfig("https://ifconfig.me/ip"),
 )
 
 
-async def aget(sess: ClientSession, url) -> dict:
+async def aget(sess: ClientSession, config: RemoteApiConfig) -> dict:
     try:
-        res = await sess.get(url)
+        res: ClientResponse = await sess.get(config.url)
     except ClientConnectionError as e:
         return {"error": str(e)}
     except Exception as e:
         return {"error": f"Unknown request error: {e}"}
 
     try:
-        return await res.json()
+        return {"value": await config.extract_result(res)}
     except JSONDecodeError as e:
         return {"error": f"Unable to parse result as JSON: {res.content} ({e})"}
     except Exception as e:
@@ -53,12 +56,11 @@ async def aget(sess: ClientSession, url) -> dict:
 
 async def get_publics_ips(ip_configs: Iterable[RemoteApiConfig]):
     async with ClientSession() as sess:
-        coros = (
-            aget(sess, config.url) for config in ip_configs
+        results: tuple[dict] = await asyncio.gather(
+            *(aget(sess, config) for config in ip_configs)
         )
-        results: tuple[dict] = await asyncio.gather(*coros)
         result = {
-            urllib.parse.urlparse(config.url).hostname: result_dict.get("error") or config.extractor(result_dict)
+            urllib.parse.urlparse(config.url).hostname: result_dict.get("error") or result_dict["value"]
             for config, result_dict in zip(ip_configs, results)
         }
         return result
